cd2723bc478a3d811058854bb6dc31903ff21eef
zja <zja0809@foxmail.com>
2017-08-18 14:51:52 +0800

    add config file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from celery import Celery
+from config import *
 """
 celery -A app worker --loglevel=info
 """
-cel = Celery('app_name', backend='redis://:myredis-aly@119.23.213.35:6379/0',broker='redis://:myredis-aly@119.23.213.35:6379/0', include=['example.task1'])
+cel = Celery('app_name', **REDIS_CONFIG)
